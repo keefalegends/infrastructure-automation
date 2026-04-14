0a1924ee5d2ae04061ebe40df75fa16dfde6ea1f
keefalegends <121414474+keefalegends@users.noreply.github.com>
2026-04-14 15:47:44 +0700

    Update outputs.tf

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,15 +1,15 @@
 output "vpc_id" {
-  value = aws_vpc.this.id
+  value = aws_vpc.lks_vpc.id
 }
 output "public_subnet_ids" {
   value = aws_subnet.public[*].id
 }
 output "private_subnet_ids" {
-  value = aws_subnet.private[*].id
+  value = aws_subnet.private-1[*].id
 }
 output "isolated_subnet_ids" {
-  value = aws_subnet.isolated[*].id
+  value = aws_subnet.isolated-1[*].id
 }
 output "private_route_table_id" {
-  value = aws_route_table.private.id
+  value = aws_route_table.private-rt.id
 }
